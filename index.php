dbd4ef84c8f1e6731b58b7f826569d71dc9cc419
pondsj62 <kantinan121050@gmail.com>
2025-07-01 11:01:28 +0700

    Update

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">   
+</head>
+<body>
+    <h2>Welcome to the IoT Data Page</h2>
+    <p class="content">This page is designed to display IoT data collected from various sensors.</p>
+    <div id="bbb">
+        โดย กันตินันท์ ภัคพรหมินทร์
+    </div>
+
+<div class="button-group">
+    <button class="button" onclick="location.href='Form_users.php'">สมัครสมาชิก</button>
+    <button class="success" onclick="location.href='Form_iot.php'">เพิ่มข้อมูล IoT</button>
+</div>
+    
+</body>
+</html>
